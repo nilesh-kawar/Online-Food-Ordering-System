885ee1cc6a0a2daa6e1b6006f2c2c98cece09a94
lordskyzw <68661064+lordskyzw@users.noreply.github.com>
2022-04-03 15:15:28 +0200

    Ready for experimental deployment

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Jhatpat Foods</title>
+	<title>Tarmicas Kitchen</title>
 	<!-- font awesome -->
   	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
   	<!-- Bootstrap -->
